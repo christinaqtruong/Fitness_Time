b3e81d1f94e70b855dcf8e974d02f9b7322b074b
Nikita Mehata <nikitamehata23@gmail.com>
2019-06-17 13:31:18 -0500

    some inputs

--- assets/main.js
+++ assets/main.js
@@ -321,26 +321,5 @@ function reset(){
     $("#restInterval-display").text("00:00");
 
 }
-var timer = new Timer();
-$('#chronoExample .startButton').click(function () {
-    timer.start();
-});
-$('#chronoExample .pauseButton').click(function () {
-    timer.pause();
-});
-$('#chronoExample .stopButton').click(function () {
-    timer.stop();
-});
-$('#chronoExample .resetButton').click(function () {
-    timer.reset();
-});
-timer.addEventListener('secondsUpdated', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
-timer.addEventListener('started', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
-timer.addEventListener('reset', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
+
     
