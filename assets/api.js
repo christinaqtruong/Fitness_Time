b29c7021e0ec8fc0dfce15bde829ddd5859f5d29
Mad Marine <pnnroman92@gmail.com>
2019-06-17 13:07:15 -0500

    add function to select tracks on napster api

--- assets/api.js
+++ assets/api.js
@@ -10,4 +10,13 @@ $("#music-button").on("click", function (event) {
     })
         .then(function (response) {
             console.log(response);
-        });})
+        });
+
+
+const tracksTemplateSource = document.getElementById('tracks-template').innerHTML;
+const tracksTemplate = Handlebars.compile(tracksTemplateSource);
+
+const $tracks = $('#tracks-container');
+
+const getTopTracks = $.get('https://api.napster.com/v2.1/tracks/top?apikey=MTlhZTJjNGUtZWYyZC00ZTkwLWI3M2UtOGJlYWM0OTQ0MmEw');
+    })
