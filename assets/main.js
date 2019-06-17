59f082c1a81f959a3c0b2da13cd2550e9e310b42
Christina Truong <christinaqtruong@gmail.com>
2019-06-17 14:08:00 -0500

    fixed timer display issue

--- assets/main.js
+++ assets/main.js
@@ -383,29 +383,3 @@ function reset(){
     // console.log("What is the current work out interval? " + workoutInterval);
     // console.log("What is the current rest interval? " + restInterval);
 }
-
-<<<<<<< HEAD
-// var timer = new Timer();
-// $('#chronoExample .startButton').click(function () {
-//     timer.start();
-// });
-// $('#chronoExample .pauseButton').click(function () {
-//     timer.pause();
-// });
-// $('#chronoExample .stopButton').click(function () {
-//     timer.stop();
-// });
-// $('#chronoExample .resetButton').click(function () {
-//     timer.reset();
-// });
-// timer.addEventListener('secondsUpdated', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-// });
-// timer.addEventListener('started', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-// });
-// timer.addEventListener('reset', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-=======
-    
->>>>>>> master
